7395c8ab67ad81dc12d03fc2d0e9bd20630c901f
Mario Alan Ramirez V <smarv2@hotmail.com>
2021-06-05 19:30:38 -0500

    inicio

---
+++ src/com/digitaltonemx/eclipse/main/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * (#)Principal.java 1.00 5 jun. 2021
+ * 
+ * Copyright (c) 2021 SURA Mexico. Derechos reservados. https://www.suramexico.com/afore/
+ */
+package com.digitaltonemx.eclipse.main;
+
+/**
+ * 	Mario Alan Ramirez Vazquez.
+ *	@author MXI01020253A
+ *  @version 1.00, 5 jun. 2021
+ */
+public class Principal {
+	public static void main(String[] args) {
+		System.out.println("olis");
+	}
+}
